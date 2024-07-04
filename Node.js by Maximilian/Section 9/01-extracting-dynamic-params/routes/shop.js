5955cda4f1a5a78fb5e601de07de29e6b466b769
Aditya <adityavarma525@gmail.com>
2024-07-04 12:14:31 +0530

    Section 9

---
+++ Node.js by Maximilian/Section 9/01-extracting-dynamic-params/routes/shop.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+const express = require('express');
+
+const shopController = require('../controllers/shop');
+
+const router = express.Router();
+
+router.get('/', shopController.getIndex);
+
+router.get('/products', shopController.getProducts);
+
+//always follow order, if we have router.get('/products/delete') at line 15, it will never pass throught line 14 because of params we are passing.
+router.get('/products/:productId', shopController.getProduct);
+
+router.get('/cart', shopController.getCart);
+
+router.get('/orders', shopController.getOrders);
+
+router.get('/checkout', shopController.getCheckout);
+
+module.exports = router;
